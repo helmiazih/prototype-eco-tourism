cf54b8bfe22f319975a9a11895a543c98a8cec77
hakimhambali <63507567+hakimhambali@users.noreply.github.com>
2020-04-11 23:34:51 +0800

    hakimhambali77@gmail.com

---
+++ lib/hiking.dart
@@ -0,0 +1,244 @@
+import 'package:flutter/material.dart';
+
+class Hiking extends StatefulWidget {
+  @override
+  _HikingState createState() => _HikingState();
+}
+
+class _HikingState extends State<Hiking> {
+   List<Container> daftarSuperhero = new List();
+
+  var karakter = [
+    {"nama": "Broga", "gambar": "1h.jpg"},
+    {"nama": "Bukit Keluang", "gambar": "2h.jpg"},
+    {"nama": "Gunung QS", "gambar": "3h.jpg"},
+    {"nama": "Cameron Highland", "gambar": "4h.jpg"},
+    {"nama": "Pinang Hill", "gambar": "5h.jpg"},
+    {"nama": "Gunung Pulai", "gambar": "6h.jpg"},
+    
+  ];
+
+  _buatlist() async {
+    for (var i = 0; i < karakter.length; i++) {
+      final karakternya = karakter[i];
+      final String gambar = karakternya["gambar"];
+
+      daftarSuperhero.add(new Container(
+          padding: new EdgeInsets.all(10.0),
+          child: new Card(
+              child: new Column(
+            children: <Widget>[
+              new Hero(
+                tag: karakternya['nama'],
+                child: new Material(
+                  child: new InkWell(
+                    onTap: () =>
+                        Navigator.of(context).push(new MaterialPageRoute(
+                              builder: (BuildContext context) => new Detail(
+                                    nama: karakternya['nama'],
+                                    gambar: gambar,
+                                  ),
+                            )),
+                    child: new Image.asset(
+                      "img/$gambar",
+                      fit: BoxFit.cover,
+                      height: 120,
+
+                    ),
+                  ),
+                ),
+              ),
+              new Padding(
+                padding: new EdgeInsets.all(10.0),
+              ),
+              new Text(
+                karakternya['nama'],
+                style: new TextStyle(fontSize: 20.0),
+              )
+            ],
+          ))));
+    }
+  }
+
+  @override
+  void initState() {
+    _buatlist();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(
+          "Hiking Place",
+          style: new TextStyle(color: Colors.white),
+        ),
+      ),
+      body: new GridView.count(
+        crossAxisCount: 2,
+        children: daftarSuperhero,
+      ),
+    );
+  }
+}
+
+class Detail extends StatelessWidget {
+  Detail({this.nama, this.gambar});
+  final String nama;
+  final String gambar;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new ListView(
+        children: <Widget>[
+          new Container(
+              height: 240.0,
+              child: new Hero(
+                tag: nama,
+                child: new Material(
+                  child: new InkWell(
+                    child: new Image.asset(
+                      "img/$gambar",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              )),
+          new BagianNama(
+            nama: nama,
+          ),
+          new BagianIcon(),
+          new Keterangan(),
+        ],
+      ),
+    );
+  }
+}
+
+class BagianNama extends StatelessWidget {
+  BagianNama({this.nama});
+  final String nama;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      
+      padding: new EdgeInsets.all(10.0),
+      child: new Row(
+        children: <Widget>[
+          new Expanded(
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Text(
+                  nama,
+                  style: new TextStyle(fontSize: 20.0, color: Colors.blue),
+                ),
+                new Text(
+                  "$nama\@gmail.com",
+                  style: new TextStyle(fontSize: 17.0, color: Colors.grey),
+                ),
+              ],
+            ),
+          ),
+          new Row(
+            children: <Widget>[
+                new IconButton(
+                icon: new Icon(Icons.star),
+                iconSize: 30.0,
+                color: Colors.red,
+                onPressed: () {
+                  SnackBar snackBar = SnackBar(
+                    content: Text("Add this place into your favourite"),
+                    backgroundColor: Colors.green,
+                    action: new SnackBarAction(
+                        label: 'Yes',
+                        textColor: Colors.white,
+                        onPressed: () {
+                          print('press Ok');
+                        }),
+                  );
+                  Scaffold.of(context).showSnackBar(snackBar);
+                },
+              ),
+               new Text(
+                "12",
+                style: new TextStyle(fontSize: 18.0),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class BagianIcon extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      padding: new EdgeInsets.all(10.0),
+      child: new Row(
+        children: <Widget>[
+          new Iconteks(
+            icon: Icons.call,
+            teks: "Call",
+          ),
+          new Iconteks(
+            icon: Icons.message,
+            teks: "Message",
+          ),
+          new Iconteks(
+            icon: Icons.photo,
+            teks: "Photo",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Iconteks extends StatelessWidget {
+  Iconteks({this.icon, this.teks});
+  final IconData icon;
+  final String teks;
+  @override
+  Widget build(BuildContext context) {
+    return new Expanded(
+      child: new Column(
+        children: <Widget>[
+          new Icon(
+            icon,
+            size: 50.0,
+            color: Colors.blue,
+          ),
+          new Text(
+            teks,
+            style: new TextStyle(fontSize: 18.0, color: Colors.blue),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Keterangan extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      padding: new EdgeInsets.all(10.0),
+      child: new Card(
+        child: new Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: new Text(
+            "Detail ...",
+            style: new TextStyle(fontSize: 18.0),
+            textAlign: TextAlign.justify,
+          ),
+        ),
+      ),
+    );
+  }
+}
